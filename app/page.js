8d82226739f54620c297bc45ab96c29cad5892ee
Sodoo615 <sodoo8.se@gmail.com>
2025-10-23 18:19:31 +0800

    sdfs

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center text-[50px]">
-      Home page
+      batuka page
     </div>
   );
 }
